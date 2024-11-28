6775971e71384a18fdd6a61f8aaacb2acc505b06
Oksana Korobanova (Vitol) <58445023+OksanaKorobanova@users.noreply.github.com>
2024-11-28 17:13:42 +0100

    added cta, footer sections

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,9 +1,33 @@
+import logo from '@/assets/images/logo.svg';
+import Image from 'next/image';
+
 const footerLinks = [
-    { href: "#", label: "Contact" },
-    { href: "#", label: "Privacy Policy" },
-    { href: "#", label: "Terms & Conditions" },
+  { href: '#', label: 'Contact' },
+  { href: '#', label: 'Privacy Policy' },
+  { href: '#', label: 'Terms & Conditions' },
 ];
 
-export default function Footer() {
-    return <div>Footer</div>;
-}
+const Footer = () => {
+  return (
+    <footer className='py-16'>
+      <div className='container'>
+        <div className='flex flex-col md:flex-row items-center md:justify-between gap-6'>
+          <div>
+            <Image src={logo} alt='logo' />
+          </div>
+          <div>
+            <nav className='flex gap-6'>
+              {footerLinks.map(({ href, label }) => (
+                <a key={label} href={href} className='text-white/50 text-sm'>
+                  {label}
+                </a>
+              ))}
+            </nav>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
